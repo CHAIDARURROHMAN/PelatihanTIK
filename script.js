3ffa856c981150a5810db3ebcf2992ddecd30d75
CHAIDARURROHMAN <chaidarrohman0@gmail.com>
2025-10-14 11:56:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-cons tombolSapa = document.getElementById('sapaButton');//pilih tombol berdasarkan ID-Nya.
+const tombolSapa = document.getElementById('sapaButton');//pilih tombol berdasarkan ID-Nya.
 tombolSapa.addEventListener('click',function(){//tambahkan 'event listener'untuk aksi 'click'.
   alert('Halo!,Thanks ya udah mampir!!');//jalankan kode ini ketika tombol di klik.
 });
